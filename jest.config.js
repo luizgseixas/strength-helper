205fe18852339b05d1a9887d0e401e4cad5bac0d
Luiz Seixas <ticoluizgustavo@outlook.com>
2024-05-24 13:35:09 -0300

    chore: config jest coverage

--- jest.config.js
+++ jest.config.js
@@ -3,13 +3,21 @@ module.exports = {
   coverageProvider: 'babel',
   testEnvironment: 'node',
   collectCoverageFrom: [
-    '<rootDir>/src/**/*.ts'
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/**/index.ts',
+    '!<rootDir>/src/factories/*.ts',
+    '!<rootDir>/src/http/resolvers/*.ts',
+    '!<rootDir>/src/http/typeDefs/*.ts',
+    '!<rootDir>/src/http/server.ts',
+    '!<rootDir>/src/infra/db/connection.ts',
+    '!<rootDir>/src/infra/db/migrate.ts',
+    '!<rootDir>/src/infra/db/schema/*.ts'
   ],
   moduleFileExtensions: ['js', 'ts', 'd.ts', 'json'],
   transform: {
-    '^.+\\.ts$': '@swc/jest',
+    '^.+\\.ts$': '@swc/jest'
   },
   testMatch: ['**/*.(spec|test).ts'],
-  bail: false,
+  bail: false
 };
 // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
